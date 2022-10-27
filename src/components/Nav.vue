c9c570b79c4e3b654260294dfafd4d793947ba00
hyo-web <dudal5236@naver.com>
2022-10-27 19:18:03 +0900

    .

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,9 +3,9 @@
         <ul>
             <li><a href="#" v-scroll-to="'#about'">About</a></li>
             <li class="active"><a href="#" v-scroll-to="'#portfolio'">Portfolio</a></li>
-            <li>
+           <!--  <li>
                 <a href="#" v-scroll-to="'#visual'"><strong>TOP</strong></a>
-            </li>
+            </li> -->
         </ul>
     </nav>
 </template>
